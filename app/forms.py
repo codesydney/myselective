d09a2559ce8c91bb559d9bdf1a89bf7f5711e961
Engramar Bollas <engramar.bollas@gmail.com>
2017-03-19 08:34:05 +1100

    Score Computation

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,18 @@
 from flask_wtf import Form
-from wtforms import TextField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
 from flask.ext.wtf import Form
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms import validators
-from wtforms.fields.html5 import EmailField
-
+from wtforms.validators import DataRequired
 
 class MainForm(Form):
-    SchoolAssessmentTitle = TextField("School Assessment")
+    SchoolAssessmentTitle = StringField("School Assessment")
     School_English = IntegerField('English', [validators.Required("Please enter your English School Assessment.")])
     School_Maths = IntegerField('Mathematics', [validators.Required("Please enter your Maths School Assessment.")])
     
-    ExaminationScoresTitle = TextField("Examination Scores")
-    ExaminationScores_English = IntegerField('English', validators=[DataRequired()])
-    ExaminationScores_Maths = IntegerField('Maths', validators=[DataRequired()])
-    ExaminationScores_GA = IntegerField('GA', validators=[DataRequired()])
-    ExaminationScores_Writing = IntegerField('Writing', validators=[DataRequired()])
+    ExaminationScoresTitle = StringField("Examination Scores")
+    ExaminationScores_English = IntegerField('English', [validators.Required("Please enter your English Exam Score.")])
+    ExaminationScores_Maths = IntegerField('Maths', [validators.Required("Please enter your Maths Exam Score.")])
+    ExaminationScores_GA = IntegerField('GA', [validators.Required("Please enter your GA Exam Score.")])
+    ExaminationScores_Writing = IntegerField('Writing', [validators.Required("Please enter your Writing Exam Score.")])
     
     submit = SubmitField('Run')
-    
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from .forms import MainForm
 
@@ -6,6 +6,41 @@ from .forms import MainForm
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = MainForm()
-    return render_template('mainform.html',
-                            title='Main Form',
-                            form=form)
+    
+    #if request.method == 'POST':
+    if form.validate_on_submit():
+        School_English = form.School_English.data
+        form.School_English.data = ''
+       
+        School_Maths = form.School_Maths.data
+        form.School_Maths.data = ''
+        
+        ExaminationScores_English = form.ExaminationScores_English.data
+        form.ExaminationScores_English.data = ''
+        
+        ExaminationScores_Maths = form.ExaminationScores_Maths.data
+        form.ExaminationScores_Maths.data = ''
+
+        ExaminationScores_GA = form.ExaminationScores_GA.data
+        form.ExaminationScores_GA.data = ''
+
+        ExaminationScores_Writing = form.ExaminationScores_Writing.data
+        form.ExaminationScores_Writing.data = ''
+        
+        Total_English = (School_English + (ExaminationScores_English * 0.667 + ExaminationScores_Writing * 0.333)) / 2
+        Total_Maths = (School_Maths + ExaminationScores_Maths) / 2
+        Total_GA = ExaminationScores_GA
+        Total_Score = Total_English + Total_Maths + Total_GA
+        
+        return render_template('result.html',
+                                form=form,
+                                School_English=School_English,
+                                School_Maths=School_Maths,
+                                ExaminationScores_English=ExaminationScores_English,
+                                ExaminationScores_Maths=ExaminationScores_Maths,
+                                ExaminationScores_GA=ExaminationScores_GA,
+                                ExaminationScores_Writing=ExaminationScores_Writing,
+                                Total_Score=Total_Score)
+    else:    
+        return render_template('mainform.html',
+                                form=form)
