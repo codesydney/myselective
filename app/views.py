3cd844cb0d6c39371e6c1e33edc79c61b87708cc
Engramar Bollas <engramar.bollas@gmail.com>
2017-04-01 23:52:48 +1100

    added try again button

--- app/views.py
+++ app/views.py
@@ -1,49 +1,53 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, url_for
 from app import app, db, models
-from .forms import MainForm
+from .forms import MainForm, ResultForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = MainForm()    
-    
-    if form.validate_on_submit():
-
-        School_English = form.School_English.data
-        form.School_English.data = ''
+	form = MainForm()
+	resultform = ResultForm()
+	
+	if form.validate_on_submit() and form.Submit1.data:
+		School_English = form.School_English.data
+		form.School_English.data = ''
        
-        School_Maths = form.School_Maths.data
-        form.School_Maths.data = ''
+		School_Maths = form.School_Maths.data
+		form.School_Maths.data = ''
         
-        ExaminationScores_English = form.ExaminationScores_English.data
-        form.ExaminationScores_English.data = ''
+		ExaminationScores_English = form.ExaminationScores_English.data
+		form.ExaminationScores_English.data = ''
         
-        ExaminationScores_Maths = form.ExaminationScores_Maths.data
-        form.ExaminationScores_Maths.data = ''
+		ExaminationScores_Maths = form.ExaminationScores_Maths.data
+		form.ExaminationScores_Maths.data = ''
 
-        ExaminationScores_GA = form.ExaminationScores_GA.data
-        form.ExaminationScores_GA.data = ''
+		ExaminationScores_GA = form.ExaminationScores_GA.data
+		form.ExaminationScores_GA.data = ''
 
-        ExaminationScores_Writing = form.ExaminationScores_Writing.data
-        form.ExaminationScores_Writing.data = ''
+		ExaminationScores_Writing = form.ExaminationScores_Writing.data
+		form.ExaminationScores_Writing.data = ''
         
-        Total_English = (School_English + (ExaminationScores_English * 0.667 + ExaminationScores_Writing * 0.333)) / 2
-        Total_Maths = (School_Maths + ExaminationScores_Maths) / 2
-        Total_GA = ExaminationScores_GA
-        Total_Score = Total_English + Total_Maths + Total_GA
+		Total_English = (School_English + (ExaminationScores_English * 0.667 + ExaminationScores_Writing * 0.333)) / 2
+		Total_Maths = (School_Maths + ExaminationScores_Maths) / 2
+		Total_GA = ExaminationScores_GA
+		Total_Score = Total_English + Total_Maths + Total_GA
         
-        schools = models.School.query.filter(models.School.entryscore <= Total_Score).order_by(models.School.entryscore.desc()).all()
-                 
-        return render_template('result.html',
-                                form=form,
-                                School_English=School_English,
-                                School_Maths=School_Maths,
-                                ExaminationScores_English=ExaminationScores_English,
-                                ExaminationScores_Maths=ExaminationScores_Maths,
-                                ExaminationScores_GA=ExaminationScores_GA,
-                                ExaminationScores_Writing=ExaminationScores_Writing,                                								
-                                Total_Score=int(Total_Score),
-                                schools=schools)
-    else:    
-        return render_template('mainform.html',
+		HomeAddress = form.HomeAddress.data
+		
+		schools = models.School.query.filter(models.School.entryscore <= Total_Score).order_by(models.School.entryscore.desc()).all()
+		return render_template('result.html',
+								resultform=resultform,
+								School_English=School_English,
+								School_Maths=School_Maths,
+								ExaminationScores_English=ExaminationScores_English,
+								ExaminationScores_Maths=ExaminationScores_Maths,
+								ExaminationScores_GA=ExaminationScores_GA,
+								ExaminationScores_Writing=ExaminationScores_Writing,                                								
+								Total_Score=int(Total_Score),
+								HomeAddress=HomeAddress,
+								schools=schools)
+	elif resultform.validate_on_submit() and resultform.Submit2.data:	
+         return redirect(url_for('index'))
+	else:		
+	    return render_template('mainform.html',
                                 form=form)
