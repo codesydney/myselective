247b799ec3cbc80e9aeb48c7107288c9285dbf00
Engramar Bollas <engramar.bollas@gmail.com>
2017-04-03 21:16:58 +1000

    Added school website, address and geocode

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from app import db
 
 class School(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    schoolname = db.Column(db.String(60), index=True, unique=True)
-    entryscore = db.Column(db.Integer, index=True, unique=False)
-
-    def __repr__(self):
-        return '{} {}'.format(self.schoolname, self.entryscore)
+	id = db.Column(db.Integer, primary_key=True)
+	schoolname = db.Column(db.String(60), index=True, unique=True)
+	entryscore = db.Column(db.Integer, index=True, unique=False)
+	website = db.Column(db.String(60), index=True, unique=True)
+	schooladdress = db.Column(db.String(60), index=True, unique=True)
+	schoollat = db.Column(db.Float, index=True, unique=True)
+	schoollng = db.Column(db.Float, index=True, unique=True)
+	
+	def __repr__(self):
+		return '{} {} {} {} {} {}'.format(self.schoolname, self.entryscore, self.website, self.schooladdress, self.schoollat, self.schoollng)
