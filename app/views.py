b58ba64e2ed1f15e3665a973522299cfdf6bc0c3
Engramar Bollas <engramar.bollas@gmail.com>
2017-04-08 15:47:52 +1000

    Added distance computation

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, request, url_for
 from app import app, db, models
 from .forms import MainForm, ResultForm
+from math import sin, cos, sqrt, atan2
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -37,6 +38,20 @@ def index():
 		HomeLng = form.HomeLng.data		
 		
 		schools = models.School.query.filter(models.School.entryscore <= Total_Score).order_by(models.School.entryscore.desc()).all()
+		R = 6373.0
+		
+		school_list = []
+		
+		for school in schools:
+			dlon = school.schoollng - float(form.HomeLng.data)	
+			dlat = school.schoollat - float(form.HomeLat.data)			
+			a = sin(dlat / 2)**2 + cos(float(form.HomeLat.data)) * cos(float(school.schoollat)) * sin(dlon / 2)**2
+			c = 2 * atan2(sqrt(a), sqrt(1 - a))
+			distance = round((R * c) / 100,2) 			
+			selected_fields=[school.schoolname,school.entryscore,school.website,distance]
+			school_list.append(selected_fields)
+		
+			
 		return render_template('result.html',
 								resultform=resultform,
 								School_English=School_English,
@@ -49,7 +64,9 @@ def index():
 								HomeAddress=HomeAddress,
 								HomeLat=HomeLat,
 								HomeLng=HomeLng,
-								schools=schools)
+								schools=schools,
+								school_list=school_list)								
+								
 	elif resultform.validate_on_submit() and resultform.Submit2.data:	
          return redirect(url_for('index'))
 	else:		
